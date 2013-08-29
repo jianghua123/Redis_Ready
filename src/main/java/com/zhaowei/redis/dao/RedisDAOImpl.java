ee0664385e834cb44775f82818d79c76a69dd7c4
zwbn57175596 <eblis.e.a.zhao@gmail.com>
2013-08-29 17:51:18 +0800

    spring-framework struts2 dwr log4j

--- src/main/java/com/zhaowei/redis/dao/RedisDAOImpl.java
+++ src/main/java/com/zhaowei/redis/dao/RedisDAOImpl.java
@@ -5,18 +5,49 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import redis.clients.jedis.ShardedJedis;
+
 import com.zhaowei.redis.factory.JedisFactory;
 import com.zhaowei.redis.util.DB;
 
 public class RedisDAOImpl {
 	
+	public boolean setName(String name) {
+		ShardedJedis jedis = null;
+		boolean result = false;
+		try {
+			jedis = JedisFactory.getJedis();
+			jedis.set("name", name);
+			result = true;
+			return result;
+		} finally {
+			JedisFactory.releaseShardedJedis(jedis);
+		}
+	}
+
+	public String getName() {
+		ShardedJedis jedis = null;
+		String result = null;
+		try {
+			jedis = JedisFactory.getJedis();
+			result = jedis.get("name");
+			return result;
+		} finally {
+			JedisFactory.releaseShardedJedis(jedis);
+		}
+	}
+
 	public List<List<String>> activityCount(String date) {
 		String cache_key = "com.zhaowei.redis.dao.redisdaoimpl.activityCouont." + date;
-		JedisFactory.getJedis().
+		ShardedJedis jedis = JedisFactory.getJedis();
+		if (null != jedis) {
+
+		}
+		JedisFactory.releaseShardedJedis(jedis);
 		List<List<String>> result = new ArrayList<List<String>>();
 		try {
 			ResultSet rs = DB.getInstance().querySql("select * from user");
-			while(rs.next()) {
+			while (rs.next()) {
 				List<String> record = new ArrayList<String>();
 				record.add(rs.getString("USERID"));
 				record.add(rs.getString("USERNAME"));
--- src/main/java/com/zhaowei/redis/service/impl/RedisServiceImpl.java
+++ src/main/java/com/zhaowei/redis/service/impl/RedisServiceImpl.java
@@ -2,14 +2,19 @@ package com.zhaowei.redis.service.impl;
 
 import java.util.List;
 
+import com.zhaowei.redis.dao.RedisDAOImpl;
 import com.zhaowei.redis.service.RedisService;
 import com.zhaowei.redis.util.RedisUtil;
 
 public class RedisServiceImpl implements RedisService {
+	
+	private RedisDAOImpl dao = new RedisDAOImpl();
 
 	@Override
 	public List<List<String>> activityCount(String date) {
 		return null;
 	}
+	
+	
 
 }
